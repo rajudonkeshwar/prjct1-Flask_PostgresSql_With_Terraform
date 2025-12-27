cb1d05399dfdc2405ea2436976dfb6a6c640f6b3
Raju Donkeshwar <rajudonkeshwar1911@gmail.com>
2025-12-27 16:22:10 +0530

    i added something

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "flask_ec2" {
   ami           = "ami-0ecb62995f68bb549" # Ubuntu
   instance_type = "t2.micro"
   subnet_id     = module.vpc.public_subnets[0]
-  key_name      = "Python"
+  key_name      = "jenkins"
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   associate_public_ip_address = true
